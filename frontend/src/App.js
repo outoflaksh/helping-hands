8de009fad1e4c2b341380c43bff21d6b83d4bd11
laksh <lakshyam38@gmail.com>
2022-03-19 15:58:31 +0530

    global styles+font

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Pages from "./pages/Pages";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Pages />
       </BrowserRouter>
     </div>
