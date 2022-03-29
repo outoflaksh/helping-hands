7ffa99f6888f8da962283517cafd8720eeabf52a
laksh <lakshyam38@gmail.com>
2022-03-29 22:53:07 +0530

    page component

--- frontend/src/components/styles/Utility.styled.jsx
+++ frontend/src/components/styles/Utility.styled.jsx
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 
+export const Page = styled.main`
+  width: 100%;
+  min-height: 100vh;
+`;
+
 export const Flex = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: ${(props) => (props.direction ? props.direction : "row")};
   justify-content: ${(props) => (props.justify ? props.justify : "flex-start")};
