bb675ee3c9a51c3b35bd3949aa91768a7ed04be8
laksh <lakshyam38@gmail.com>
2022-03-29 22:52:07 +0530

    form styled redesign

--- frontend/src/components/styles/Form.styled.jsx
+++ frontend/src/components/styles/Form.styled.jsx
@@ -2,26 +2,38 @@ import styled from "styled-components";
 
 export const FormGroup = styled.div`
   display: flex;
-  justify-content: space-between;
-  width: 100%;
+  flex-direction: column;
+  margin-top: 15px;
+
+  input {
+    padding: 15px;
+    background-color: #43ffcb;
+    border-style: none;
+    border-radius: 3px;
+  }
 `;
 
 export const FormContainer = styled.div`
-  width: 400px;
-  height: 500px;
-  margin: 1rem;
+  width: 55%;
 `;
 
 export const FormHeader = styled.div`
   width: 100%;
-  text-align: center;
-  padding: 0.5rem;
-  background-color: whitesmoke;
+  text-align: left;
+  padding: 0.5rem 0;
+
+  h1 {
+    font-weight: 600;
+    font-size: 3rem;
+  }
 `;
 
 export const FormContent = styled.div`
   height: 100%;
   width: 100%;
-  padding: 0.5rem;
-  background-color: wheat;
+  padding: 25px;
+  border-radius: 25px;
+  background-color: #b7ffeb;
 `;
+
+export const SubmitButton = styled.button``;
