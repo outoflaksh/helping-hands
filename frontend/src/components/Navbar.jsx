1dab0db42b941d48c3d44a662ebc1348eeb1fe5b
laksh <lakshyam38@gmail.com>
2022-04-08 11:56:37 +0530

    explicit home link on nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,26 +14,23 @@ import {
 
 const Navbar = () => {
   const { currentUser } = useContext(AuthContext);
-  const usersCollectionRef = collection(FireStoreDB, 'users');
+  const usersCollectionRef = collection(FireStoreDB, "users");
   const [userData, setUserData] = useState();
   useEffect(() => {
     const checkDetails = async () => {
       const userDocs = await getDocs(usersCollectionRef);
-      setUserData(
-        () => {
-          let userData;
-          for (let doc of userDocs.docs) {
-            let docData = doc.data();
-            if (docData.uid && docData.uid == currentUser.uid) {
-              userData = docData;
-            }
+      setUserData(() => {
+        let userData;
+        for (let doc of userDocs.docs) {
+          let docData = doc.data();
+          if (docData.uid && docData.uid == currentUser.uid) {
+            userData = docData;
           }
-          return userData;
         }
-      );
+        return userData;
+      });
     };
-    if (currentUser)
-      checkDetails();
+    if (currentUser) checkDetails();
   }, [currentUser]);
   return (
     <Nav>
@@ -42,34 +39,32 @@ const Navbar = () => {
       </NavLink>
       <NavList>
         <NavItem>
+          <NavLink to="/">Home</NavLink>
+        </NavItem>
+        <NavItem>
           <NavLink to="/jobs">Jobs</NavLink>
         </NavItem>
-        {
-          currentUser ?
-            <>
+        {currentUser ? (
+          <>
+            <NavItem>
+              <NavLink to="/profile">Profile</NavLink>
+            </NavItem>
+            {userData && userData.level == "client" ? (
               <NavItem>
-                <NavLink to="/profile">Profile</NavLink>
+                <NavLink to="/jobs/new">Post Job</NavLink>
               </NavItem>
-              {
-                userData && userData.level == "client" ?
-                  <NavItem>
-                    <NavLink to="/jobs/new">Post Job</NavLink>
-                  </NavItem>
-                  : undefined
-              }
-            </>
-            :
-            <>
-              <NavItem>
-                <NavLink to="/auth/register">Register</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink to="/auth/login">Login</NavLink>
-              </NavItem>
-
-
-            </>
-        }
+            ) : undefined}
+          </>
+        ) : (
+          <>
+            <NavItem>
+              <NavLink to="/auth/register">Register</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/auth/login">Login</NavLink>
+            </NavItem>
+          </>
+        )}
       </NavList>
     </Nav>
   );
